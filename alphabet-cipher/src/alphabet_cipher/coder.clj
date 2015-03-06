148a1044f9c909aad70b1686e7301eaca42329d3
Derek Anderson <derek@armyofevilrobots.com>
2015-03-06 12:38:37 -0800

    My first stab at alphabet cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,27 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(def alphabet (mapv char (range (int \a) (+ 1 (int \z)))))
 
-(defn decode [keyword message]
-  "decodeme")
+(def cipher (mapv (fn [x] 
+                    (vec (concat (drop x alphabet) (take x alphabet)))) 
+                  (range 26)))
+
+(defn char-ofs [the-char]
+  (- (int the-char) (int \a)))
+
+(defn char-ofs-inv [the-char]
+  (mod (- (+ 1 (int \z)) (int the-char)) 26))
+
+(defn charsub [key-ofs the-char the-key]
+  (get-in cipher [(key-ofs the-key) (char-ofs the-char)]))
+
+(defn apply-cipher [key-ofs-fun keyword message]
+  (apply str 
+         (for [ofs (range (count message))]
+           ((partial charsub key-ofs-fun)
+            (nth message ofs)
+            (nth keyword (mod ofs (count keyword)))))))
+
+(def encode (partial apply-cipher char-ofs))
+(def decode (partial apply-cipher char-ofs-inv))
+
