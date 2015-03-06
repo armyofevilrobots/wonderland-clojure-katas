3d82515b8aa71fef0ddb48fcad9335959eace3dc
Derek Anderson <derek@armyofevilrobots.com>
2015-03-06 12:59:52 -0800

    Add private declarations.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -6,16 +6,16 @@
                     (vec (concat (drop x alphabet) (take x alphabet)))) 
                   (range 26)))
 
-(defn char-ofs [the-char]
+(defn- char-ofs [the-char]
   (- (int the-char) (int \a)))
 
-(defn char-ofs-inv [the-char]
+(defn- char-ofs-inv [the-char]
   (mod (- (+ 1 (int \z)) (int the-char)) 26))
 
-(defn charsub [key-ofs the-char the-key]
+(defn- charsub [key-ofs the-char the-key]
   (get-in cipher [(key-ofs the-key) (char-ofs the-char)]))
 
-(defn apply-cipher [key-ofs-fun keyword message]
+(defn- apply-cipher [key-ofs-fun keyword message]
   (apply str 
          (for [ofs (range (count message))]
            ((partial charsub key-ofs-fun)
